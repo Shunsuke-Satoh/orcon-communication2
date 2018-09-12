1e897d87a5c2797a2a3bc584b5d559674354f358
ONE NATION <onenation@wannomacbook-puro.local>
2018-09-12 12:05:26 +0900

    Initial Commit

---
+++ orcon-communicationTests/orcon_communicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  orcon_communicationTests.swift
+//  orcon-communicationTests
+//
+//  Created by ONE NATION on 2018/09/12.
+//  Copyright © 2018年 ONE NATION. All rights reserved.
+//
+
+import XCTest
+@testable import orcon_communication
+
+class orcon_communicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
